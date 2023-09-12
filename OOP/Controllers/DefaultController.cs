d331855de3395ecba004c0dfc24632d627147b28
Can Çilek <46296419+CanCilek@users.noreply.github.com>
2023-09-12 15:53:22 +0300

    Add project files.

---
+++ OOP/Controllers/DefaultController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OOP.Ornekler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OOP.Controllers
+{
+    public class DefaultController : Controller
+    {
+        //void Islemler()
+        //{
+        //    Class1 c = new Class1();
+        //    c.Topla();
+        //}
+
+        void messages()
+        {
+            ViewBag.m1 = "Merhaba Bu Bir Core Projesi";
+            ViewBag.m2 = "Merhaba Proje Çok İyi Duruyor";
+            ViewBag.m3 = "Hi!!";
+        }
+
+        int Topla()
+        {
+            int s1 = 20;
+            int s2 = 30;
+            int sonuc = s1 + s2;
+            return sonuc;
+
+        }
+
+        int Cevre()
+        {
+            int kisa = 10;
+            int uzun = 20;
+            int c = 2 * (kisa + uzun);
+            return c;
+        }
+
+        void MesajListesi(string p)
+        {
+            ViewBag.v = p;
+        }
+
+
+        public IActionResult Index()
+        {
+
+            messages();
+            MesajListesi("Parametre İsmi");
+            return View();
+        }
+
+        public IActionResult Urunler()
+        {
+            messages();
+            ViewBag.t = Topla();
+            ViewBag.c = Cevre();
+            return View();
+
+        }
+
+        public IActionResult Musteriler()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ OOP/Ornekler/Class1.cs
@@ -0,0 +1,22 @@
+﻿using OOP.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OOP.Ornekler
+{
+    public class Class1
+    {
+
+        public void Topla()
+        {
+            int sayi1 = 10;
+            int sayi2 = 20;
+            int sonuc = sayi1 + sayi2;
+        }
+
+        
+
+    }
+}
