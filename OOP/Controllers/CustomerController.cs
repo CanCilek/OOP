4890415b1301b8b6a1cf523169a67060c965bea9
Can Çilek <46296419+CanCilek@users.noreply.github.com>
2023-09-13 23:49:17 +0300

    ReFactoring İçin Yeni Projeye Başlandı

--- OOP/Controllers/CustomerController.cs
+++ OOP/Controllers/CustomerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using OOP.Entity;
 using OOP.ProjeContext;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -21,6 +22,61 @@ namespace OOP.Controllers
             var values = context.Customers.ToList();
 
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddCustomer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCustomer(Customer c)
+        {
+            if(c.Name.Length >= 6 && c.City != "" && c.City.Length >= 3)
+            {
+                context.Add(c);
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+
+            
+        }
+        public IActionResult DeleteCustomer(int Id)
+        {
+            var value = context.Customers.Where(x => x.Id == Id).FirstOrDefault();
+            context.Customers.Remove(value);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateCustomer(int id)
+        {
+            var value = context.Customers.Where(x => x.Id == id).FirstOrDefault();
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateCustomer(Customer c)
+        {
+
+            if (c.Name.Length >= 6 && c.City != "" && c.City.Length >= 3)
+            {
+                var value = context.Customers.Where(x => x.Id == c.Id).FirstOrDefault();
+                value.Name = c.Name;
+                value.City = c.City;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
